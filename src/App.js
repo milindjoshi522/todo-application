818c977df5f3be0bcb46a551f3acc025da7dccd4
Milind Joshi <milindjoshi@Milinds-MacBook-Pro.local>
2021-09-27 17:59:11 +0530

    Changes : 	A) Edited readme file. 	B) modified components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./todoComponents/Header";
+import { Todos } from "./todoComponents/Todos";
+import { Footer } from "./todoComponents/Footer";
+import { AddTodoItem } from "./todoComponents/AddTodoItem";
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const onDelete=(todo)=>{
+    console.log("func on delete of todo ", todo);
+
+    setTodos(todos.filter((e)=>{
+      return e!==todo ;
+    }))
+  }
+
+  const addTodo=(title, desc) => {
+    console.log("adding a todo")
+    let sno;
+    if(todos.length==0) {
+      sno=0;
+    } else {
+     sno = todos[todos.length-1].sno + 1;
+    }
+
+
+    const addedTodo={
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+setTodos([...todos, addedTodo]);
+    console.log("adding ", addedTodo);
+  }
+ 
+
+  const [todos, setTodos] = useState([
+    {
+      sno:1,
+      title: "todo item 1",
+      descritption: "todo description 1",
+    },
+    {
+      sno:2,
+      title: "todo item 2",
+      descritption: "todo description 2",
+    },{
+      sno:3,
+      title: "todo item 3",
+      descritption: "todo description 3",
+    },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header title="Todo List" searchBar={true}/>
+    <AddTodoItem addTodo={addTodo}/>
+    <Todos todos={todos} onDelete={onDelete}/>
+    <Footer/>
+    </>
+    
   );
 }
 
